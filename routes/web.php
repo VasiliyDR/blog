3cd1ff4ce122d820d47e9938e52133ed9dce892b
uservasian <vasilek96@bk.ru>
2022-05-04 18:43:01 +0300

    adding and connecting Admin Lte panel

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,13 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/',  IndexController::class );
 });
 
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function() {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/',  IndexController::class );
+    });
+
+});
+
 Auth::routes();
 
 
