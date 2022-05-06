0e042209d5114a6caa06ca96530ec18a03892723
uservasian <vasilek96@bk.ru>
2022-05-06 17:20:55 +0300

    incomplete CRUD for users in admin panel

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,16 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::delete('/{tag}', DestroyController::class)->name('admin.tag.destroy');
     });
 
+    Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
+        Route::get('/',  IndexController::class )->name('admin.user.index');
+        Route::get('/create', CreateController::class)->name('admin.user.create');
+        Route::post('/', StoreController::class)->name('admin.user.store');
+        Route::get('/{user}', ShowController::class)->name('admin.user.show');
+        Route::get('/{user}/edit', EditController::class)->name('admin.user.edit');
+        Route::patch('/{user}', UpdateController::class)->name('admin.user.update');
+        Route::delete('/{user}', DestroyController::class)->name('admin.user.destroy');
+    });
+
 });
 
 Auth::routes();
