5ed5fdc7b537cab6361a8b0626647293d64fcbcc
uservasian <vasilek96@bk.ru>
2022-05-09 00:22:07 +0300

    adding likes to posts

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -19,6 +19,28 @@
             </section>
             <div class="row">
                 <div class="col-lg-9 mx-auto">
+                    <section class="py-3">
+                        @auth() 
+                        <form action="{{ route('post.like.store', $post->id) }}" method="post">
+                            @csrf
+                            <span>{{ $post->liked_users_count }}</span>
+                            <button type="submit" class="border-0 bg-transparent">
+                                    @if(auth()->user()->likedPosts->contains($post->id))
+                                        <i class="fas fa-heart"></i>
+                                    @else
+                                        <i class="far fa-heart"></i>
+                                    @endif
+                            </button>
+                        </form>
+                        @endauth
+                        @guest()
+                            <div>
+                                <span>{{ $post->liked_users_count }}</span>
+                                <i class="far fa-heart"></i>
+                            </div>
+                        @endguest
+                    </section>
+                    @if($relatedPosts->count() > 0)
                     <section class="related-posts">
                         <h2 class="section-title mb-4" data-aos="fade-up">Похожие посты</h2>
                         <div class="row">
@@ -31,6 +53,8 @@
                             @endforeach
                         </div>
                     </section>
+                    @endif
+
                     <section class="comment-list mb-4 ">
 
                         <h2 class="section-title mb-5" data-aos="fade-up">Комментарии ({{ $post->comments->count() }})</h2>
